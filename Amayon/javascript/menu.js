b4a472936646c4f59b20a0e739de579915e0c2d6
MaFan8 <maple_f@hotmail.com>
2020-12-05 19:57:34 -0500

    fixed toggle and dropdown menu

--- Amayon/javascript/menu.js
+++ Amayon/javascript/menu.js
@@ -5,9 +5,18 @@ trig.addEventListener("click", openNav);
 closer.addEventListener("click", closeNav);
 
 function openNav() {
-  document.getElementById("myNav").style.width = "100%";
+  document.getElementById("myNav").style.width = "400px";
 }
 
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
+}
+
+const dropclick = document.querySelector("#dropdown");
+
+dropclick.addEventListener("click", openToggle);
+
+function openToggle() {
+  const opencontent = document.querySelector("#dropdown-content");
+  opencontent.style.display = "block";
 }
